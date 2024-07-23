d1d0c9b6b25e17fb7561f34288c518fa3886e375
Void <84394631+VoidDominator@users.noreply.github.com>
2024-07-23 17:12:28 +0800

    Partitioning and moving lots of things around to port the original plain html to vue
    Known issue: AB testing does not work yet

---
+++ src/components/TitleBar.vue
@@ -0,0 +1,13 @@
+<template>
+    <header class="mx-auto pt-10 lg:pt-6 lg:px-8 w-240 lg:w-full mb-8">
+        <h1 class="inline-block sm:block sm:mb-2 font-light text-60 lg:text-4xl text-black-dark leading-tight mr-2">
+            <span class="inline-block">欢迎回来!</span>
+            <span class="code-label">Error code 3933: Repeating Media</span>
+        </h1>
+        <div>
+            Visit <a class="KnowledgeBaseLink" target="_self" rel="noopener noreferrer">our knowledge base</a> for more
+            information.
+        </div>
+        <div class="mt-3">9999-99-99 99:99:99 UTC+99</div>
+    </header>
+</template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,29 @@
 <template>
-  <HelloWorld />
+  <div id="cf-wrapper">
+    <div id="cf-error-details" class="p-0">
+      <TitleBar />
+      <SystemStatus />
+      <DescriptionContent />
+      <v-footer>
+        <RayFooter />
+      </v-footer>
+    </div>
+  </div>
 </template>
 
 <script setup>
-  //
+//
+</script>
+
+<script>
+  function ab() {
+    if (Math.round(Math.random())) {
+      return "https://voiddominator.github.io/help.kevinhu.world/";
+    } else {
+      return "https://iokb.wuff.dog";
+    }
+  }
+  document.querySelectorAll(".KnowledgeBaseLink").forEach(function (link) {
+    link.href = ab();
+  });
 </script>
