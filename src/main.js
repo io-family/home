2dfe09190dad70e70d86a5e5cd1257261bdb4efd
Void <84394631+VoidDominator@users.noreply.github.com>
2024-07-26 11:28:00 +0800

    Implemented internationalization with vue-i18n, follows browser language

--- src/main.js
+++ src/main.js
@@ -12,9 +12,23 @@ import App from './App.vue'
 
 // Composables
 import { createApp } from 'vue'
+import { createI18n } from 'vue-i18n'
+
+// locale files
+import en_ca from '@/assets/i18n/en-ca.json'
+import zh_cn from '@/assets/i18n/zh-cn.json'
 
 const app = createApp(App)
+const i18n = createI18n({
+    legacy: false,
+    locale: navigator.language.toLowerCase(),
+    fallbackLocale: 'en-ca',
+    messages: {
+        'en-ca': en_ca,
+        'zh-cn': zh_cn
+    }
+})
 
 registerPlugins(app)
-
+app.use(i18n)
 app.mount('#app')
