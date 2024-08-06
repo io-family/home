3f6cc3f25bccbcd8d49abff5e82c78718d0cde24
Void <84394631+VoidDominator@users.noreply.github.com>
2024-08-06 15:00:41 +0800

    Indentation formats, version is now 1.2.1

--- src/components/DescriptionContent.vue
+++ src/components/DescriptionContent.vue
@@ -1,32 +1,32 @@
 <template>
-    <div class="w-240 lg:w-full mx-auto mb-8 lg:px-8">
-        <div class="clearfix">
-            <div class="w-1/2 md:w-full float-left pr-6 md:pb-10 md:pr-0 leading-relaxed">
-                <h2 class="text-3xl font-normal leading-1.3 mb-4">{{ $t("what_happened_text") }}</h2>
-                <p class="mb-6">{{ $t("what_happened_description") }}</p>
+  <div class="w-240 lg:w-full mx-auto mb-8 lg:px-8">
+    <div class="clearfix">
+      <div class="w-1/2 md:w-full float-left pr-6 md:pb-10 md:pr-0 leading-relaxed">
+        <h2 class="text-3xl font-normal leading-1.3 mb-4">{{ $t("what_happened_text") }}</h2>
+        <p class="mb-6">{{ $t("what_happened_description") }}</p>
 
-                <h3 class="text-15 font-semibold mb-2">{{ $t("welcome_text") }}</h3>
-                <p class="mb-6">{{ $t("welcome_description") }}</p>
+        <h3 class="text-15 font-semibold mb-2">{{ $t("welcome_text") }}</h3>
+        <p class="mb-6">{{ $t("welcome_description") }}</p>
 
-                <h3 class="text-15 font-semibold mb-2">{{ $t("future_plans_text") }}</h3>
-                <p class="mb-6">{{ $t("future_plans_description") }}</p>
-            </div>
+        <h3 class="text-15 font-semibold mb-2">{{ $t("future_plans_text") }}</h3>
+        <p class="mb-6">{{ $t("future_plans_description") }}</p>
+      </div>
 
-            <div class="w-1/2 md:w-full float-left leading-relaxed">
-                <h2 class="text-3xl font-normal leading-1.3 mb-4">{{ $t("instructions_text") }}</h2>
-                <h3 class="text-15 font-semibold mb-2">{{ $t("condition_rainborn") }}</h3>
-                <p class="mb-6">{{ $t("rainborn_description") }}</p>
+      <div class="w-1/2 md:w-full float-left leading-relaxed">
+        <h2 class="text-3xl font-normal leading-1.3 mb-4">{{ $t("instructions_text") }}</h2>
+        <h3 class="text-15 font-semibold mb-2">{{ $t("condition_rainborn") }}</h3>
+        <p class="mb-6">{{ $t("rainborn_description") }}</p>
 
-                <h3 class="text-15 font-semibold mb-2">{{ $t("condition_visitor") }}</h3>
-                <p class="mb-6">{{ $t("visitor_description") }}</p>
+        <h3 class="text-15 font-semibold mb-2">{{ $t("condition_visitor") }}</h3>
+        <p class="mb-6">{{ $t("visitor_description") }}</p>
 
-                <h3 class="text-15 font-semibold mb-2">{{ $t("condition_ab_tester") }}</h3>
-                <p class="mb-6">{{ $t("ab_tester_description") }}</p>
+        <h3 class="text-15 font-semibold mb-2">{{ $t("condition_ab_tester") }}</h3>
+        <p class="mb-6">{{ $t("ab_tester_description") }}</p>
 
-                <h3 class="text-15 font-semibold mb-2">{{ $t("condition_owner") }}</h3>
-                <p><span>{{ $t("owner_description") }}</span> <a rel="noopener noreferrer"
-                        class="KnowledgeBaseLink">{{ $t("troubleshoot_link_text") }}</a></p>
-            </div>
-        </div>
+        <h3 class="text-15 font-semibold mb-2">{{ $t("condition_owner") }}</h3>
+        <p><span>{{ $t("owner_description") }}</span> <a rel="noopener noreferrer" class="KnowledgeBaseLink">{{
+          $t("troubleshoot_link_text") }}</a></p>
+      </div>
     </div>
+  </div>
 </template>
--- src/components/TitleBar.vue
+++ src/components/TitleBar.vue
@@ -1,12 +1,13 @@
 <template>
-    <header class="mx-auto pt-10 lg:pt-6 lg:px-8 w-240 lg:w-full mb-8">
-        <h1 class="inline-block sm:block sm:mb-2 font-light text-60 lg:text-4xl text-black-dark leading-tight mr-2">
-            <span class="inline-block">{{ $t("title_welcome_back") }}</span>
-            <span class="code-label">{{ $t("error_code_label") }}</span>
-        </h1>
-        <div>
-            {{ $t("information_link_before") }}<a class="KnowledgeBaseLink" target="_self" rel="noopener noreferrer">{{ $t("information_link_text") }}</a>{{ $t("information_link_after") }}
-        </div>
-        <div class="mt-3">9999-99-99 99:99:99 UTC+99</div>
-    </header>
+  <header class="mx-auto pt-10 lg:pt-6 lg:px-8 w-240 lg:w-full mb-8">
+    <h1 class="inline-block sm:block sm:mb-2 font-light text-60 lg:text-4xl text-black-dark leading-tight mr-2">
+      <span class="inline-block">{{ $t("title_welcome_back") }}</span>
+      <span class="code-label">{{ $t("error_code_label") }}</span>
+    </h1>
+    <div>
+      {{ $t("information_link_before") }}<a class="KnowledgeBaseLink" target="_self"
+        rel="noopener noreferrer">{{ $t("information_link_text") }}</a>{{ $t("information_link_after") }}
+    </div>
+    <div class="mt-3">9999-99-99 99:99:99 UTC+99</div>
+  </header>
 </template>
